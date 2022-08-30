628b4a6b5ba5b7ab8e35492d05dec3d24c6aef38
Jhonny Göransson <jhonny.goransson@gmail.com>
2022-08-30 23:05:56 +0200

    Initial commit

---
+++ vantage/vantage_core.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.VANTAGE_W       = hash("VANTAGE_W")
+M.VANTAGE_S       = hash("VANTAGE_S")
+M.VANTAGE_A       = hash("VANTAGE_A")
+M.VANTAGE_D       = hash("VANTAGE_D")
+M.VANTAGE_Q       = hash("VANTAGE_Q")
+M.VANTAGE_E       = hash("VANTAGE_E")
+M.VANTAGE_MOUSE_1 = hash("VANTAGE_MOUSE_1")
+
+return M
